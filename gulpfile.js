7a8591c0e81d015aae2dc6ea6cc6db753a055ddb
Ricardo Linck <ricardo.linck@retailinmotion.com>
2018-05-28 21:37:14 +0100

    First components

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var config= {
     paths:{
         html: './src/*.html',
         js: './src/**/*.js',
+        images:'./src/images/*',
         css: [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -67,4 +68,10 @@ gulp.task('css', function(){
         .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('images', function(){
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist+'/images'))
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'js', 'images', 'css', 'open', 'watch']);
